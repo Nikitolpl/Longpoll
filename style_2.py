42827eaa477bd86f0c511efb90f068a5ee914149
Nikitolpl <64868009+Nikitolpl@users.noreply.github.com>
2020-05-09 02:58:24 +0300

    Update style_2.py

--- style_2.py
+++ style_2.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'file_2.ui'
-#
-# Created by: PyQt5 UI code generator 5.14.0
-#
-# WARNING! All changes made in this file will be lost!
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
